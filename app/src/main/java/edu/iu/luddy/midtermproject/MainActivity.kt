c86969a07b53af843a3c0a488271f222130536ef
pgudapat <pgudapat@iu.edu>
2025-10-11 16:39:52 -0400

    Midterm UI done

---
+++ app/src/main/java/edu/iu/luddy/midtermproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.iu.luddy.midtermproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+}
